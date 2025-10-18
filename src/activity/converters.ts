c2168a6b6ce618126910ecbebb3ef1222e70a93c
GEARLESS JOE <c1094282069@gmail.com>
2025-10-19 00:46:25 +0800

    version: 0.0.20

--- src/activity/converters.ts
+++ src/activity/converters.ts
@@ -2,8 +2,18 @@ import { BaseActivityConverter, ActivityContext } from "./base.js";
 import { FileType, TokenAST } from "../types.js";
 import { ActivityType, ActivityRecordType, ActivityLapType } from "../types.js";
 import { GPX11Type, TrksegType, TrkType, WptType } from "../GPX/types.js";
-import { FITFileType, RecordMesgType, LapMesgType, SessionMesgType } from "../FIT/types.js";
-import { ActivityType as TCXActivityType, ActivityLapType as TCXActivityLapType, TCXFileType, TrackpointType } from "../TCX/types.js";
+import {
+  FITFileType,
+  RecordMesgType,
+  LapMesgType,
+  SessionMesgType,
+} from "../FIT/types.js";
+import {
+  ActivityType as TCXActivityType,
+  ActivityLapType as TCXActivityLapType,
+  TCXFileType,
+  TrackpointType,
+} from "../TCX/types.js";
 import { semicirclesToDegrees, convertGPXExtensionsMapping } from "../util.js";
 import dayjs from "dayjs";
 import { MetricsAggregator } from "./metrics/MetricsCalculator.js";
@@ -55,24 +65,20 @@ export class GPXToActivityConverter extends BaseActivityConverter {
 
     // Convert routes
     if (gpx.rte) {
-      obj.routes = gpx.rte.map((rte, idx) => { 
-          return this.convertTrackSeg2Lap(rte, idx)
-       })
+      obj.routes = gpx.rte.map((rte, idx) => {
+        return this.convertTrackSeg2Lap(rte, idx);
+      });
     }
     return obj;
   }
 
   /**
    * 将 Wpt 转为 ActivityRecordType
-   * @param point 
-   * @param idx 
-   * @returns 
+   * @param point
+   * @param idx
+   * @returns
    */
-  protected convertPoint(
-    point: WptType,
-    idx?: number
-  ): ActivityRecordType {
-
+  protected convertPoint(point: WptType, idx?: number): ActivityRecordType {
     const { extensions } = point;
     const gpxExtensions = extensions
       ? this.convertGPXExtensions(extensions)
@@ -93,58 +99,62 @@ export class GPXToActivityConverter extends BaseActivityConverter {
   }
   /**
    * 将 trk 转为 Activity
-   * @param trk 
-   * @param idx 
-   * @returns 
+   * @param trk
+   * @param idx
+   * @returns
    */
   private convertTrk2Activity(trk: TrkType, idx: number): ActivityType {
-
-    const laps = trk.trkseg?.map((trkseg, segIdx) =>
-      this.convertTrackSeg2Lap(trkseg, segIdx)
-    ) || [];
+    const laps =
+      trk.trkseg?.map((trkseg, segIdx) =>
+        this.convertTrackSeg2Lap(trkseg, segIdx)
+      ) || [];
     // 聚合数据
-    const aggregatedData = new MetricsAggregator().calculateLapAggMetrics(laps) as SessionMesgType;
+    const aggregatedData = new MetricsAggregator().calculateLapAggMetrics(
+      laps
+    ) as SessionMesgType;
     return {
       index: idx,
       messageIndex: idx,
       laps: laps,
       ...aggregatedData,
     };
-  } 
+  }
   /**
    * 将 trkseg 转为 lap
-   * @param seg 
-   * @param idx 
-   * @returns 
+   * @param seg
+   * @param idx
+   * @returns
    */
   private convertTrackSeg2Lap(seg: TrksegType, idx: number): ActivityLapType {
-    const points = seg.trkpt?.filter(
-            (point: WptType | undefined) => point !== undefined && point.time)
-            ?.map((trkpt: WptType, ptIdx: number) =>
-            this.convertPoint(trkpt, ptIdx)
-          )
-      || [];
+    const points =
+      seg.trkpt
+        ?.filter(
+          (point: WptType | undefined) => point !== undefined && point.time
+        )
+        ?.map((trkpt: WptType, ptIdx: number) =>
+          this.convertPoint(trkpt, ptIdx)
+        ) || [];
 
     const metricsAggregator = new MetricsAggregator();
     const aggregatedData = metricsAggregator.calculateRecordsAggMetrics(points);
 
     return {
       index: idx,
-      messageIndex:idx,
+      messageIndex: idx,
       ...aggregatedData,
       records: aggregatedData?.records || [],
     };
   }
   /**
    * 将 GPX 的 wpt[] 转为 ActivityRecordType 数组
-   * @param wpts 
-   * @returns 
+   * @param wpts
+   * @returns
    */
   private convertWpts2Points(wpts: WptType[]): ActivityRecordType[] {
     const points: ActivityRecordType[] = [];
     for (let i = 0; i < wpts.length; i++) {
       const point = this.convertPoint(wpts[i], i);
-        if (point) points.push(point);
+      if (point) points.push(point);
     }
     return points;
   }
@@ -203,7 +213,10 @@ export class FITToActivityConverter extends BaseActivityConverter {
           ? round(semicirclesToDegrees(Number(sessionMesg.startPositionLat)), 6)
           : undefined,
         startPositionLong: sessionMesg?.startPositionLong
-          ? round(semicirclesToDegrees(Number(sessionMesg.startPositionLong)), 6)
+          ? round(
+              semicirclesToDegrees(Number(sessionMesg.startPositionLong)),
+              6
+            )
           : undefined,
         endPositionLat: sessionMesg?.endPositionLat
           ? round(semicirclesToDegrees(Number(sessionMesg.endPositionLat)), 6)
@@ -224,9 +237,9 @@ export class FITToActivityConverter extends BaseActivityConverter {
   }
   /**
    * 将 FIT 的 recordMesg 转为 ActivityRecordType
-   * @param point 
-   * @param idx 
-   * @returns 
+   * @param point
+   * @param idx
+   * @returns
    */
   protected convertPoint(
     point: RecordMesgType,
@@ -244,37 +257,49 @@ export class FITToActivityConverter extends BaseActivityConverter {
 
     return {
       index: idx || 0,
-      positionLat: positionLat ? round(semicirclesToDegrees(Number(positionLat)), 6) : undefined,
-      positionLong: positionLong ? round(semicirclesToDegrees(Number(positionLong)), 6) : undefined,
+      positionLat: positionLat
+        ? round(semicirclesToDegrees(Number(positionLat)), 6)
+        : undefined,
+      positionLong: positionLong
+        ? round(semicirclesToDegrees(Number(positionLong)), 6)
+        : undefined,
       altitude: altitude || enhancedAltitude || undefined,
       speed: speed || enhancedSpeed || undefined,
       ...rest,
     };
   }
   /**
-   * 
-   * @param laps 
-   * @returns 
+   *
+   * @param laps
+   * @returns
    */
   private convertFITLap2Trackseg(laps?: LapMesgType[]): ActivityLapType[] {
     const routeSeg: ActivityLapType[] = [];
     if (!laps?.length) return [];
-    for(let i = 0; i < laps.length; i++) {
+    for (let i = 0; i < laps.length; i++) {
       const lap = laps[i];
       const records =
         lap?.recordMesgs
           ?.map((record, idx) => this.convertPoint(record, idx))
           .filter((point) => point !== undefined) || [];
-      
+
       routeSeg.push({
         index: i,
         ...lap,
         // 对坐标数据进行转换
-        startPositionLat: lap?.startPositionLat ? round(semicirclesToDegrees(Number(lap.startPositionLat)), 6): undefined,
-        startPositionLong: lap?.startPositionLong ? round(semicirclesToDegrees(Number(lap.startPositionLong)), 6) : undefined,
-        endPositionLat: lap?.endPositionLat ? round(semicirclesToDegrees(Number(lap.endPositionLat)), 6) : undefined,
-        endPositionLong: lap?.endPositionLong ? round(semicirclesToDegrees(Number(lap.endPositionLong)), 6) : undefined,
-       
+        startPositionLat: lap?.startPositionLat
+          ? round(semicirclesToDegrees(Number(lap.startPositionLat)), 6)
+          : undefined,
+        startPositionLong: lap?.startPositionLong
+          ? round(semicirclesToDegrees(Number(lap.startPositionLong)), 6)
+          : undefined,
+        endPositionLat: lap?.endPositionLat
+          ? round(semicirclesToDegrees(Number(lap.endPositionLat)), 6)
+          : undefined,
+        endPositionLong: lap?.endPositionLong
+          ? round(semicirclesToDegrees(Number(lap.endPositionLong)), 6)
+          : undefined,
+
         records: records,
       });
     }
@@ -338,13 +363,19 @@ export class TCXToActivityConverter extends BaseActivityConverter {
   }
   /**
    * 将 tcx 的 activity 转为 ActivityType
-   * @param activity 
+   * @param activity
    */
-  private convertTCXActivityToActivity(activity: TCXActivityType, idx: number): ActivityType {
-
-    const laps = activity.Lap?.map((lap, segIdx) => this.convertTCXLap2Lap(lap, segIdx)) || [];
+  private convertTCXActivityToActivity(
+    activity: TCXActivityType,
+    idx: number
+  ): ActivityType {
+    const laps =
+      activity.Lap?.map((lap, segIdx) => this.convertTCXLap2Lap(lap, segIdx)) ||
+      [];
     // 聚合数据
-    const aggregatedData = new MetricsAggregator().calculateLapAggMetrics(laps) as SessionMesgType;
+    const aggregatedData = new MetricsAggregator().calculateLapAggMetrics(
+      laps
+    ) as SessionMesgType;
     return {
       index: idx,
       messageIndex: idx,
@@ -355,35 +386,36 @@ export class TCXToActivityConverter extends BaseActivityConverter {
 
   /**
    * 将 TCX Lap 转为 ActivityLapType
-   * @param lap 
-   * @param idx 
-   * @returns 
+   * @param lap
+   * @param idx
+   * @returns
    */
-  private convertTCXLap2Lap(lap: TCXActivityLapType, idx:number): ActivityLapType {
+  private convertTCXLap2Lap(
+    lap: TCXActivityLapType,
+    idx: number
+  ): ActivityLapType {
     // 将 track point 提取出来
-    const allPoints = lap?.Track?.flatMap(track => track.Trackpoint) || [];
-    const points = allPoints
-      ?.filter(
-        (point: TrackpointType | undefined): point is TrackpointType =>
-          point !== undefined &&
-          point.Time !== undefined
-      )
-      .map((point: TrackpointType, ptIdx: number) =>
-        this.convertPoint(point, ptIdx)
-      )
-      || [];
+    const allPoints = lap?.Track?.flatMap((track) => track.Trackpoint) || [];
+    const points =
+      allPoints
+        ?.filter(
+          (point: TrackpointType | undefined): point is TrackpointType =>
+            point !== undefined && point.Time !== undefined
+        )
+        .map((point: TrackpointType, ptIdx: number) =>
+          this.convertPoint(point, ptIdx)
+        ) || [];
 
     const metricsAggregator = new MetricsAggregator();
     const aggregatedData = metricsAggregator.calculateRecordsAggMetrics(points);
 
     return {
       index: idx,
-      messageIndex:idx,
+      messageIndex: idx,
       ...aggregatedData,
       records: aggregatedData?.records || [],
     };
   }
-
 
   protected convertPoint(
     point: TrackpointType,
@@ -393,8 +425,12 @@ export class TCXToActivityConverter extends BaseActivityConverter {
 
     return {
       index: idx || 0,
-      positionLat: Position?.LatitudeDegrees ? Number(Position!.LatitudeDegrees) : undefined,
-      positionLong: Position?.LongitudeDegrees ? Number(Position!.LongitudeDegrees) : undefined,
+      positionLat: Position?.LatitudeDegrees
+        ? Number(Position!.LatitudeDegrees)
+        : undefined,
+      positionLong: Position?.LongitudeDegrees
+        ? Number(Position!.LongitudeDegrees)
+        : undefined,
       timestamp: dayjs(Time).toDate(),
       distance: DistanceMeters,
       altitude: AltitudeMeters,
